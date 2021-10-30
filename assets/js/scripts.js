4f693fddbe1a8afe44e074f2d15445414e402368
Querobim Damasceno <damascenoquerobim@gmail.com>
2021-10-30 18:36:01 -0300

    Add EventListener for banner dots

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,19 +4,19 @@ let posBanner = 0;
 let posDot = 0;
 
 banner.style.transition = '2s ease';
-selectDot[0].classList.toggle('active');
+selectDot[0].classList.add('active');
 
  function bannerAnimation() {
-    posBanner -= 100;
-    posDot++;
-    if(posBanner < -200) {
-        cleanActive(posDot)
+    if(posBanner === -200) {
+        cleanActive()
         posBanner = 0;
         posDot = 0;
         banner.style.transform = `translateX(${posBanner}vw)`;
         selectDot[0].classList.toggle('active');
     } else {
-        cleanActive(posDot);
+        posBanner -= 100;
+        posDot = posBanner / -100;
+        cleanActive();
         changeActive(posDot);
         banner.style.transform = `translateX(${posBanner}vw)`;
         console.log(posDot);
@@ -25,14 +25,37 @@ selectDot[0].classList.toggle('active');
 } 
 
 function changeActive(pos) {
-    selectDot[pos].classList.toggle('active');
+    selectDot[pos].classList.add('active');
 }
 
-function cleanActive(pos){
-    selectDot[pos - 1].classList.toggle('active');
+function cleanActive(){
+    for(let i = 0; i < selectDot.length; i++){
+        selectDot[i].classList.remove('active');
+    }
 }
 
-
+for(let i = 0; i < selectDot.length; i++){
+    selectDot[i].addEventListener('click', () => {
+        if(i === 0) {
+            posBanner = 0;
+            cleanActive();
+            changeActive(i);
+            banner.style.transform = `translateX(${posBanner}vw)`;
+        }
+        if(i === 1) {
+            posBanner = -100;
+            cleanActive();
+            changeActive(i);
+            banner.style.transform = `translateX(${posBanner}vw)`;
+        }
+        if(i === 2){
+            posBanner = -200;
+            cleanActive();
+            changeActive(i);
+            banner.style.transform = `translateX(${posBanner}vw)`;
+        }
+    });
+}
 
 
 setInterval(bannerAnimation, 5000);
